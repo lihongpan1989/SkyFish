6dffc6186623f7ca17d38bfc283012422be865cb
lihongpan1989 <562608410@qq.com>
2015-11-18 10:47:02 +0800

    Initial Commit

---
+++ SkyFish/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SkyFish
+//
+//  Created by 李洪攀 on 15/11/18.
+//  Copyright © 2015年 SkyFish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SkyFish/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SkyFish
+//
+//  Created by 李洪攀 on 15/11/18.
+//  Copyright © 2015年 SkyFish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
